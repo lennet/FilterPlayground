f12544e017186b0d4d0f5e311440b735f850666d
Leonard Thomas <leo.thomas@me.com>
2017-08-23 23:06:18 +0200

    improved float picker slider interaction

--- FilterPlayground/Attributes View/CircularSlider.swift
+++ FilterPlayground/Attributes View/CircularSlider.swift
@@ -38,7 +38,8 @@ class CircularSlider: UIControl {
         
         addSubview(knob)
         
-        let panGestureRecognier = UIPanGestureRecognizer(target: self, action: #selector(didPanKnob(recognizer:)))
+        let panGestureRecognier = AllTouchesPanGestureRecognizer()
+        panGestureRecognier.callBack = didPanKnob
         knob.addGestureRecognizer(panGestureRecognier)
     }
     
@@ -47,7 +48,7 @@ class CircularSlider: UIControl {
         knob.center = knobLocation(for: 270)
     }
     
-    @objc func didPanKnob(recognizer: UIPanGestureRecognizer) {
+    @objc func didPanKnob( recognizer: AllTouchesPanGestureRecognizer, state: UIGestureRecognizerState) {
         
         let location = recognizer.location(in: self)
         recognizer.setTranslation(.zero, in: self)
@@ -72,7 +73,7 @@ class CircularSlider: UIControl {
         
         previousProgress = prog
 
-        switch recognizer.state {
+        switch state {
         case .began:
             sendActions(for: .editingDidBegin)
             break
@@ -108,6 +109,35 @@ class CircularSlider: UIControl {
         let path = UIBezierPath(ovalIn: circleRect)
         path.stroke()
         path.lineWidth = lineWidth
+    }
+    
+}
+
+import UIKit.UIGestureRecognizerSubclass
+
+public class AllTouchesPanGestureRecognizer: UIPanGestureRecognizer {
+    
+    public var callBack: ((_ recognizer: AllTouchesPanGestureRecognizer, _ state: UIGestureRecognizerState) -> ())?
+    
+    public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent) {
+        super.touchesBegan(touches, with: event)
+        state = .began
+        callBack?(self, .began)
+    }
+    
+    public override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent) {
+        super.touchesMoved(touches, with: event)
+        callBack?(self, .changed)
+    }
+    
+    public override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent) {
+        super.touchesEnded(touches, with: event)
+        callBack?(self, .ended)
+    }
+    
+    public override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent) {
+        super.touchesCancelled(touches, with: event)
+        callBack?(self, .cancelled)
     }
     
 }

ffef56cf412810d7ef33d38b26c2ea3ca3aa9a67
Leonard Thomas <leo.thomas@me.com>
2017-09-25 09:51:30 +0200

    removed boilerplate code

--- FilterPlayground/Create Project/Templates/SelectTemplateTableViewController.swift
+++ FilterPlayground/Create Project/Templates/SelectTemplateTableViewController.swift
@@ -13,21 +13,6 @@ class SelectTemplateTableViewController: UITableViewController {
     let templates = TemplatesManager.getURLs()
     var didSelectTemplate: ((URL) -> Void)?
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     // MARK: - Table view data source
 
     override func numberOfSections(in _: UITableView) -> Int {

ceee3fc8970786a2853e5917c7f862bcb950d786
Leonard Thomas <leo.thomas@me.com>
2017-10-16 13:35:38 +0200

    used relative touch location for TouchDataBinding

--- FilterPlayground/Helper/OutputContainerView.swift
+++ FilterPlayground/Helper/OutputContainerView.swift
@@ -13,12 +13,19 @@ class OutputContainerView: UIView {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
         guard let touch = touches.first else { return }
-        (TouchDataBindingEmitter.shared as! TouchDataBindingEmitter).detectedTouch(point: touch.location(in: self))
+        detectedTouch(for: touch.location(in: self))
     }
 
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesMoved(touches, with: event)
         guard let touch = touches.first else { return }
-        (TouchDataBindingEmitter.shared as! TouchDataBindingEmitter).detectedTouch(point: touch.location(in: self))
+        detectedTouch(for: touch.location(in: self))
+    }
+
+    func detectedTouch(for location: CGPoint) {
+        var relativeLocation = location
+        relativeLocation.x = (relativeLocation.x / bounds.width) * 100
+        relativeLocation.y = (relativeLocation.y / bounds.height) * 100
+        (TouchDataBindingEmitter.shared as! TouchDataBindingEmitter).detectedTouch(point: relativeLocation)
     }
 }
